dcb78915b2af63b1807c30a26b12762491523502
oron01 <136211946+oron01@users.noreply.github.com>
2023-12-12 14:01:29 +0200

    add devserver option

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/index.html',
